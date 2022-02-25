66d6cb64e3b1656d23451f1ccd917b662d7868e4
KabirBhae <arafatkabirkhan22@gmail.com>
2022-02-25 18:32:17 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Home from "./Pages/Home/Home";
+// import { Person } from "@mui/icons-material";
+
+function App() {
+	return <Home />;
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+	return (
+		<div className="topBarContainer">
+			<div className="topBarLeft"></div>
+			<div className="topBarCenter"></div>
+			<div className="topBarRight"></div>
+		</div>
+	);
+};
+
+export default Navbar;
