4a53e87e9f8e4c1f90333a6939d2697a835a90c2
KabirBhae <arafatkabirkhan22@gmail.com>
2022-02-27 04:30:26 +0600

    leftbar completed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,22 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
+import Leftbar from "../../Components/Leftbar/Leftbar";
+import Feed from "../../Components/MainFeed/Feed";
+import Rightbar from "../../Components/Rightbar/Rightbar";
+
+import "./home.css";
 
 const Home = () => {
-	return <Navbar />;
+	return (
+		<>
+			<Navbar></Navbar>
+			<div className="homeContainer">
+				<Leftbar></Leftbar>
+				<Feed></Feed>
+				<Rightbar></Rightbar>
+			</div>
+		</>
+	);
 };
 
 export default Home;
