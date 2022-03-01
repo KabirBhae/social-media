6230b26fe3601ea233b1274b3e5b87781e485c08
KabirBhae <arafatkabirkhan22@gmail.com>
2022-03-02 00:14:58 +0600

    like handler used

--- src/Components/FeedContent/FeedContent.js
+++ src/Components/FeedContent/FeedContent.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./feedContent.css";
 import { MoreVert } from "@mui/icons-material";
 
 import { Users } from "../../dummydata";
 
 export default function FeedContent({ content }) {
+	const [likes, setLikes] = useState(content.like);
+	const [liked, isLiked] = useState(false);
+
+	function likeHandler() {
+		if (liked) {
+			setLikes(likes - 1);
+			isLiked(false);
+		} else {
+			setLikes(likes + 1);
+			isLiked(true);
+		}
+	}
 	return (
 		<div className="content">
 			<div className="contentWrapper">
@@ -32,11 +44,19 @@ export default function FeedContent({ content }) {
 				</div>
 				<div className="contentBottom">
 					<div className="contentBottomLeft">
-						<img className="likeIcon" src="assets/like.png" alt="" />
-						<img className="likeIcon" src="assets/heart.png" alt="" />
-						<span className="contentLikeCounter">
-							{content.like} people like it
-						</span>
+						<img
+							className="likeIcon"
+							src="assets/like.png"
+							alt=""
+							onClick={likeHandler}
+						/>
+						<img
+							className="likeIcon"
+							src="assets/heart.png"
+							alt=""
+							onClick={likeHandler}
+						/>
+						<span className="contentLikeCounter">{likes} people like it</span>
 					</div>
 					<div className="contentBottomRight">
 						<span className="contentCommentText">
